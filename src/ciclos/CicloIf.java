39e884dd7cafb8cdd830882ef7db9a61d8082531
Valeria <valerigalvan0913@gmail.com>
2023-11-15 19:41:47 -0600

    agregando clase de if

---
+++ src/ciclos/CicloIf.java
@@ -0,0 +1,15 @@
+package ciclos;
+
+public class CicloIf {
+
+	public static void main(String[] args) {
+		// test 1
+	int x=0;
+	if (x<0) {
+		System.out.println("numero negativo");
+		
+	}
+
+	}
+
+}
